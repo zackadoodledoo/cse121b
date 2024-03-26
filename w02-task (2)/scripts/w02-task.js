431265bdc294814cc74d86d33925b691b2f22e23
zackadoodledoo <97266950+zackadoodledoo@users.noreply.github.com>
2024-03-25 19:39:12 -0700

    Add files via upload

---
+++ w02-task (2)/scripts/w02-task.js
@@ -0,0 +1,29 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+
+
+
+
+/* Step 3 - Element Variables */
+
+
+
+
+
+/* Step 4 - Adding Content */
+
+
+
+
+
+
+/* Step 5 - Array */
+
+
+
+
+
+
